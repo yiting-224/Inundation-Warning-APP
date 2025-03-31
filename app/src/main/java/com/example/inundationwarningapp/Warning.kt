de3b0d53c8d92d06a1f9baef953c029f38b2ac4a
DESKTOP-TR4NGSK\蔡宜庭 <karen022493@gmail.com>
2025-03-31 11:55:33 +0800

    warning page setting

--- app/src/main/java/com/example/inundationwarningapp/Warning.kt
+++ app/src/main/java/com/example/inundationwarningapp/Warning.kt
@@ -4,23 +4,32 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.Row
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.draw.clip
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.width
+
+
 
 @Composable
 fun WarningScreen() {
@@ -33,7 +42,6 @@ fun WarningScreen() {
         Column(
             modifier = Modifier
                 .fillMaxHeight()
-            // .padding(20.dp)
         ) {
             // header
             Text(
@@ -44,6 +52,50 @@ fun WarningScreen() {
                     .fillMaxWidth(),
                 textAlign = TextAlign.Left
             )
+            Spacer(modifier = Modifier.padding(10.dp))
+            MessageCard(Message(stringResource(R.string.flood_warning), "住宅區域 1 小時後可能出現淹水情形。\n請務必採取防災措施。"))
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.TopCenter
+    ){
+        Surface(shape = MaterialTheme.shapes.large, shadowElevation = 3.dp, color = MaterialTheme.colorScheme.error) {
+
+            Column{
+                Row(modifier = Modifier.padding(top = 10.dp, bottom = 0.dp, start = 10.dp, end = 0.dp)){
+                    Image(
+                        painter = painterResource(R.drawable.flood_unselected),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(40.dp)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = msg.author,
+                        color = MaterialTheme.colorScheme.background,
+                        modifier = Modifier.padding(top = 10.dp, bottom = 0.dp, start = 2.dp, end = 10.dp),
+                        style = MaterialTheme.typography.titleMedium,
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 20.sp
+                    )
+                }
+
+
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(top = 0.dp, bottom = 10.dp, start = 10.dp, end = 10.dp),
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontSize = 18.sp
+                )
+            }
+
         }
     }
 }

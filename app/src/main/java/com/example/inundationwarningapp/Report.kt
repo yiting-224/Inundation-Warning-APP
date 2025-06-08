36683207b450a6388daa996f4e47c88b08fcf62c
DESKTOP-TR4NGSK\蔡宜庭 <karen022493@gmail.com>
2025-06-08 11:43:35 +0800

    SOS page edit

---
+++ app/src/main/java/com/example/inundationwarningapp/Report.kt
@@ -0,0 +1,23 @@
+package com.example.inundationwarningapp
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Report() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("這裡是 Report")
+    }
+}
--- app/src/main/java/com/example/inundationwarningapp/SOS.kt
+++ app/src/main/java/com/example/inundationwarningapp/SOS.kt
@@ -1,5 +1,7 @@
 package com.example.inundationwarningapp
 
+import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
@@ -7,6 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -28,6 +31,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Surface
+
 
 @Composable
 fun SOSScreen() {
@@ -51,7 +56,9 @@ fun SOSScreen() {
                     .fillMaxWidth(),
                 textAlign = TextAlign.Left
             )
+
             Spacer(modifier = Modifier.padding(15.dp))
+
             Text(
                 text = stringResource(R.string.I_am_a___),
                 style = MaterialTheme.typography.headlineSmall,
@@ -60,235 +67,186 @@ fun SOSScreen() {
                     .fillMaxWidth(),
                 textAlign = TextAlign.Right
             )
+
             Spacer(modifier = Modifier.padding(10.dp))
-            Column (
+
+            Row (
                 modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.Start
+                horizontalArrangement = androidx.compose.foundation.layout.Arrangement.SpaceBetween
             ) {
-                androidx.compose.material3.Button(
-                    onClick = { println("Customized Button Clicked!") },
-                    modifier = Modifier
-                        .size(width = 275.dp, height = 80.dp),
-                    colors = ButtonDefaults.buttonColors(
-                        contentColor = Color.White
-                    ),
-                    shape = CutCornerShape(10.dp),
+                Column (
+                    horizontalAlignment = Alignment.Start
+
                 ) {
-                    Row(
-                        modifier = Modifier.padding(
-                            top = 0.dp,
-                            bottom = 0.dp,
-                            start = 0.dp,
-                            end = 0.dp
-                        )
+                    androidx.compose.material3.Button(
+                        onClick = { println("Customized Button Clicked!") },
+                        modifier = Modifier
+                            .size(width = 180.dp, height = 180.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            contentColor = Color.White
+                        ),
+                        shape = CutCornerShape(10.dp),
                     ) {
-                        Image(
-                            painter = painterResource(R.drawable.person),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(40.dp)
-                        )
-                        Spacer(modifier = Modifier.width(27.dp))
-                        Text(
-                            stringResource(R.string.regular_user),
-                            color = MaterialTheme.colorScheme.background,
-                            modifier = Modifier.padding(
-                                top = 3.dp,
-                                bottom = 0.dp,
-                                start = 3.dp,
-                                end = 28.dp
-                            ),
-                            style = MaterialTheme.typography.titleLarge,
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 23.sp
-                        )
+                        Column {
+                            Image(
+                                painter = painterResource(R.drawable.person),
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .size(70.dp)
+                                    .align(Alignment.CenterHorizontally)
+                            )
+
+                            Text(
+                                stringResource(R.string.regular_user),
+                                color = MaterialTheme.colorScheme.background,
+                                style = MaterialTheme.typography.titleLarge,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 26.sp
+                            )
+                        }
+                    }
+                }
+
+                Column (
+                    horizontalAlignment = Alignment.End
+                ) {
+                    androidx.compose.material3.Button(
+                        onClick = { println("Customized Button Clicked!") },
+                        modifier = Modifier
+                            .size(width = 180.dp, height = 180.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            contentColor = Color.White
+                        ),
+                        shape = CutCornerShape(10.dp),
+                    ) {
+                        Column {
+                            Image(
+                                painter = painterResource(R.drawable.sos),
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .size(70.dp)
+                                    .align(Alignment.CenterHorizontally)
+                            )
+
+                            Text(
+                                stringResource(R.string.non_native_speaker),
+                                color = MaterialTheme.colorScheme.background,
+                                style = MaterialTheme.typography.titleLarge,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 22.sp
+                            )
+                        }
                     }
                 }
             }
-            Spacer(modifier = Modifier.height(45.dp))
+
+            Spacer(modifier = Modifier.height(10.dp))
+
             Column (
                 modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.End
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 androidx.compose.material3.Button(
                     onClick = { println("Customized Button Clicked!") },
                     modifier = Modifier
-                        .size(width = 275.dp, height = 80.dp),
+                        .size(width = 375.dp, height = 200.dp),
                     colors = ButtonDefaults.buttonColors(
                         contentColor = Color.White
                     ),
                     shape = CutCornerShape(10.dp),
                 ) {
-                    Row(
-                        modifier = Modifier.padding(
-                            top = 0.dp,
-                            bottom = 0.dp,
-                            start = 3.dp,
-                            end = 0.dp
-                        )
-                    ) {
-                        Image(
-                            painter = painterResource(R.drawable.sos),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(40.dp)
-                        )
-                        Spacer(modifier = Modifier.width(15.dp))
-                        Text(
-                            stringResource(R.string.non_native_speaker),
-                            color = MaterialTheme.colorScheme.background,
-                            modifier = Modifier.padding(
-                                top = 3.dp,
-                                bottom = 0.dp,
-                                start = 3.dp,
-                                end = 28.dp
-                            ),
-                            style = MaterialTheme.typography.titleLarge,
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 23.sp
-                        )
-                    }
-                }
-            }
-            Spacer(modifier = Modifier.height(45.dp))
-            Column (
-                modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.Start
-            ) {
-                androidx.compose.material3.Button(
-                    onClick = { println("Customized Button Clicked!") },
-                    modifier = Modifier
-                        .size(width = 275.dp, height = 80.dp),
-                    colors = ButtonDefaults.buttonColors(
-                        contentColor = Color.White
-                    ),
-                    shape = CutCornerShape(10.dp),
-                ) {
-                    Row(
-                        modifier = Modifier.padding(
-                            top = 0.dp,
-                            bottom = 0.dp,
-                            start = 0.dp,
-                            end = 0.dp
-                        )
-                    ) {
+                    Column {
                         Image(
                             painter = painterResource(R.drawable.blind),
                             contentDescription = null,
                             modifier = Modifier
-                                .size(40.dp)
+                                .size(70.dp)
+                                .align(Alignment.CenterHorizontally)
                         )
-                        Spacer(modifier = Modifier.width(27.dp))
+
                         Text(
                             stringResource(R.string.visually_impaired_individual),
                             color = MaterialTheme.colorScheme.background,
-                            modifier = Modifier.padding(
-                                top = 3.dp,
-                                bottom = 0.dp,
-                                start = 3.dp,
-                                end = 28.dp
-                            ),
                             style = MaterialTheme.typography.titleLarge,
                             fontWeight = FontWeight.Bold,
-                            fontSize = 23.sp
+                            fontSize = 26.sp
                         )
                     }
                 }
             }
-            Spacer(modifier = Modifier.height(45.dp))
-            Column (
-                modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.End
-            ) {
-                androidx.compose.material3.Button(
-                    onClick = { println("Customized Button Clicked!") },
-                    modifier = Modifier
-                        .size(width = 275.dp, height = 80.dp),
-                    colors = ButtonDefaults.buttonColors(
-                        contentColor = Color.White
-                    ),
-                    shape = CutCornerShape(10.dp),
-                ) {
-                    Row(
-                        modifier = Modifier.padding(
-                            top = 0.dp,
-                            bottom = 0.dp,
-                            start = 0.dp,
-                            end = 0.dp
-                        )
-                    ) {
-                        Image(
-                            painter = painterResource(R.drawable.hearing_disabled),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(40.dp)
-                        )
-                        Spacer(modifier = Modifier.width(27.dp))
-                        Text(
-                            stringResource(R.string.hearing_impaired_individual),
-                            color = MaterialTheme.colorScheme.background,
-                            modifier = Modifier.padding(
-                                top = 3.dp,
-                                bottom = 0.dp,
-                                start = 3.dp,
-                                end = 28.dp
-                            ),
-                            style = MaterialTheme.typography.titleLarge,
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 23.sp
-                        )
-                    }
-                }
-            }
-            Spacer(modifier = Modifier.height(45.dp))
-            Column (
-                modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.Start
-            ) {
-                androidx.compose.material3.Button(
-                    onClick = { println("Customized Button Clicked!") },
-                    modifier = Modifier
-                        .size(width = 275.dp, height = 85.dp),
-                    colors = ButtonDefaults.buttonColors(
-                        contentColor = Color.White
-                    ),
-                    shape = CutCornerShape(10.dp),
-                ) {
-                    Row(
-                        modifier = Modifier.padding(
-                            top = 0.dp,
-                            bottom = 0.dp,
-                            start = 0.dp,
-                            end = 0.dp
-                        )
-                    ) {
-                        Image(
-                            painter = painterResource(R.drawable.accessible),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(40.dp)
-                        )
-                        Spacer(modifier = Modifier.width(27.dp))
-                        Text(
-                            stringResource(R.string.person_with_disabilities),
-                            color = MaterialTheme.colorScheme.background,
-                            modifier = Modifier.padding(
-                                top = 3.dp,
-                                bottom = 0.dp,
-                                start = 3.dp,
-                                end = 28.dp
-                            ),
-                            style = MaterialTheme.typography.titleLarge,
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 23.sp
-                        )
-                    }
-                }
-            }
-        }
-    }
-    Box(){
 
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = androidx.compose.foundation.layout.Arrangement.SpaceBetween
+            ) {
+                Column (
+                    horizontalAlignment = Alignment.End
+                ) {
+                    androidx.compose.material3.Button(
+                        onClick = { println("Customized Button Clicked!") },
+                        modifier = Modifier
+                            .size(width = 180.dp, height = 180.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            contentColor = Color.White
+                        ),
+                        shape = CutCornerShape(10.dp),
+                    ) {
+                        Column {
+                            Image(
+                                painter = painterResource(R.drawable.hearing_disabled),
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .size(70.dp)
+                                    .align(Alignment.CenterHorizontally)
+                            )
+                            Spacer(modifier = Modifier.width(27.dp))
+                            Text(
+                                stringResource(R.string.hearing_impaired_individual),
+                                color = MaterialTheme.colorScheme.background,
+                                style = MaterialTheme.typography.titleLarge,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 26.sp
+                            )
+                        }
+                    }
+                }
+
+                Column (
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    androidx.compose.material3.Button(
+                        onClick = { println("Customized Button Clicked!") },
+                        modifier = Modifier
+                            .size(width = 180.dp, height = 180.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            contentColor = Color.White
+                        ),
+                        shape = CutCornerShape(10.dp),
+                    ) {
+                        Column {
+                            Image(
+                                painter = painterResource(R.drawable.accessible),
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .size(70.dp)
+                                    .align(Alignment.CenterHorizontally)
+                            )
+                            Spacer(modifier = Modifier.width(27.dp))
+                            Text(
+                                stringResource(R.string.person_with_disabilities),
+                                color = MaterialTheme.colorScheme.background,
+                                style = MaterialTheme.typography.titleLarge,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 26.sp
+                            )
+                        }
+                    }
+                }
+            }
+
+        }
     }
 }
 
